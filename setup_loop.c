3817e1a258c9b365a20daa6d18f08a6f50bc556f
Alexandre Carlton <acar7163@uni.sydney.edu.au>
2016-03-15 23:00:32 +1100

    Start timer only after reading package file output
    Before, we would trigger the timer callback on a certain interval
    without fail; if the time it took to return the package output was
    greater than the interval, we would have multiple files open.
    
    Now we start the io watcher, and on its callback stop the io watcher and
    start again the ev_timer; when that callback is started we stop the
    timer and start the io again.
    
    We also start the io watcher instead of the timer so we get our updates
    as soon as possible.

--- setup_loop.c
+++ setup_loop.c
@@ -1,4 +1,9 @@
+#define _XOPEN_SOURCE 700
+
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
 
 #include "setup_loop.h"
 
@@ -18,7 +23,8 @@ static ev_io bspwm_watcher;
 static ev_timer wifi_timer;
 static ev_timer battery_watcher;
 static ev_timer sound_watcher;
-static ev_timer packages_watcher;
+static ev_timer packages_timer;
+static ev_io packages_io_watcher;
 static ev_periodic datetime_watcher;
 static ev_signal signal_watcher;
 static ev_prepare prepare_display;
@@ -93,6 +99,8 @@ static void
 packages_io_cb(struct ev_loop *loop, ev_io *w, int revents)
 {
   (void) revents;
+  ev_io_stop(loop, w);
+
   FILE *packages_file = fdopen(w->fd, "r");
   PackagesInfo *info = (PackagesInfo *) w->data;
   PackagesInfo old_info = {
@@ -103,33 +111,19 @@ packages_io_cb(struct ev_loop *loop, ev_io *w, int revents)
   if (old_info.to_update != info->to_update) {
     updated = true;
   }
-
-  // We're done here, we'll let the timer kick off things again.
-  ev_io_stop(loop, w);
-  free(w);
+  ev_timer_again(loop, &packages_timer);
 }
 
-// This is called every X seconds, kicks off pacman watcher.
-// TODO: have pacman_io watcher kick off the timer, in case checkupdates
-// takes longer than the interval specified.
 static void
 packages_cb(struct ev_loop *loop, ev_timer *w, int revents)
 {
-  (void) loop;
   (void) revents;
-  //Now we activate it,
-  FILE *packages_file;
-  PackagesInfo *info = (PackagesInfo *)w->data;
-  packages_file = popen("checkupdates", "r");
-  int packages_descriptor = fileno(packages_file);
-
-  ev_io *pacman_watcher = (ev_io *) malloc(sizeof(ev_io));
-  pacman_watcher->data = (void *) info;
-
-  // Bonus: Find way to make sure we don't have one running.
-  ev_io_init(pacman_watcher, packages_io_cb, packages_descriptor, EV_READ);
-  ev_io_start(loop, pacman_watcher);
-  ev_timer_again(loop, w);
+  // We need to stop the timer in case our repeat interval is smaller than the
+  // time taken for our command to yield results.
+  ev_timer_stop(loop, w);
+  FILE *packages_file = popen("checkupdates", "r");
+  ev_io_set(&packages_io_watcher, fileno(packages_file), EV_READ);
+  ev_io_start(loop, &packages_io_watcher);
 }
 
 static void
@@ -189,25 +183,11 @@ void add_callbacks(Bar * const bar, int in_fd, int out_fd)
   ev_init(&sound_watcher, sound_cb);
   sound_watcher.repeat = 1.;
 
-  // Slight problem: This takes too long and blocks.
-  // Idea: We popen it, get the file descriptor and place a io watcher on it
-  // When readable, we get the packages, close the file, open a new one, and attach the current watcher again.
-  // Note that we need to use fcntl with O_NONBLOCK so it doesn't block.
-  // ... On second thought, is having non-blocking even necessary?
-  // Yeah, probably; file pointers are buffered while file descriptors aren't;
-  // in any case, we need to make it a descriptor.
-  //
-  // Also need to take into account that we need to cycle this.
-  // So we'd ned to make a timer, and when we activate that timer
-  // We popen, get the fd, and create an io watcher on that fd for READ.
-  // When it's ready, we call update_package_info
-  //  Then stop the watcher (free if malloc'd, have a suspicion we'd need to)
-  // Then we add a timer again on the loop.
-  //    Will we have a scoping problem? Might need to malloc it when stopping it.
-
-  packages_watcher.data = &bar->packages;
-  ev_init(&packages_watcher, packages_cb);
-  packages_watcher.repeat = 5 * 60.;
+  FILE *packages = popen("checkupdates", "r");
+  packages_io_watcher.data = &bar->packages;
+  ev_io_init(&packages_io_watcher, packages_io_cb, fileno(packages), EV_READ);
+  ev_init(&packages_timer, packages_cb);
+  packages_timer.repeat = 5 * 60.;
 
   // include an offset so that we don't get the time before it changes.
   // TODO Maybe separate into date watcher and time watcher?
@@ -235,7 +215,7 @@ void start_watchers(struct ev_loop *loop)
   ev_timer_again(loop, &wifi_timer);
   ev_timer_again(loop, &battery_watcher);
   ev_timer_again(loop, &sound_watcher);
-  ev_timer_again(loop, &packages_watcher);
+  ev_io_start(loop, &packages_io_watcher);
   // TODO Make separate watchers for time and date
   ev_periodic_start(loop, &datetime_watcher);
   ev_signal_start(loop, &signal_watcher);
