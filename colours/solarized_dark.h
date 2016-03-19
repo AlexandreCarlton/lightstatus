f287711f8100bf0f49c344f7bdd52cac26a041c2
Alexandre Carlton <acar7163@uni.sydney.edu.au>
2016-03-19 17:09:43 +1100

    Refactor colour settings into their own folder

---
+++ colours/solarized_dark.h
@@ -0,0 +1,25 @@
+
+static char const color_default_fg[]          = "#839496";
+static char const color_default_bg[]          = "#002b36";
+static char const color_monitor_fg[]          = "#839496";
+static char const color_monitor_bg[]          = "#002b36";
+static char const color_focused_monitor_fg[]  = "#fdf6e3";
+static char const color_focused_monitor_bg[]  = "#586e75";
+static char const color_free_fg[]             = "#839497";
+static char const color_free_bg[]             = "#002b36";
+static char const color_focused_free_fg[]     = "#073642";
+static char const color_focused_free_bg[]     = "#586e75";
+static char const color_occupied_fg[]         = "#fdf6e3";
+static char const color_occupied_bg[]         = "#073642";
+static char const color_focused_occupied_fg[] = "#fdf6e3";
+static char const color_focused_occupied_bg[] = "#586e75";
+static char const color_urgent_fg[]           = "#839496";
+static char const color_urgent_bg[]           = "#002b36";
+static char const color_focused_urgent_fg[]   = "#586e75";
+static char const color_focused_urgent_bg[]   = "#fdf6e3";
+static char const color_state_fg[]            = "#839496";
+static char const color_state_bg[]            = "#002b36";
+static char const color_title_fg[]            = "#839496";
+static char const color_title_bg[]            = "#002b36";
+static char const color_sys_fg[]              = "#839496";
+static char const color_sys_bg[]              = "#002b36";
